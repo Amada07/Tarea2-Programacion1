b5a7c65529b13fba7f3f916e3c22c31065bea2c8
Amada07 <amadacarcamo938@gmail.com>
2025-03-19 23:27:08 -0600

    Getters and setters
    Se crearon los getters y setters de la clase Cuenta Bancaria

--- SistemaBancario/src/sistemabancario/CuentaBancaria.java
+++ SistemaBancario/src/sistemabancario/CuentaBancaria.java
@@ -19,6 +19,28 @@ public class CuentaBancaria {
         this.numeroCuenta = numeroCuenta;
         this.saldo= 0.0; //Inicializa el saldo en 0
         this.propietario= propietario;
-        
+    } 
+    
+        //getter
+
+    public String getNumeroCuenta() {
+        return numeroCuenta;
     }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public Cliente getPropietario() {
+        return propietario;
+    }
+        
+    //Setter para propietario
+
+    public void setPropietario(Cliente propietario) {
+        this.propietario = propietario;
+    }
+    
+ 
+    
 }
