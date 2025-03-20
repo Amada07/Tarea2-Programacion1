1d0a95319fdca8b9e0bf184536da7d1391c4cad1
Amada07 <amadacarcamo938@gmail.com>
2025-03-19 23:21:39 -0600

    Atributos privados y constructor
    se crearon los atributos privados y el constructor de la clase cuenta Bancaria

---
+++ SistemaBancario/src/sistemabancario/CuentaBancaria.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistemabancario;
+
+/**
+ *
+ * @author Amada
+ */
+public class CuentaBancaria {
+    //Atributos privados
+    private String numeroCuenta;
+    private double saldo;
+    private Cliente propietario;
+    
+    //constructor
+    public CuentaBancaria(String numeroCuenta,Cliente propietario){
+        this.numeroCuenta = numeroCuenta;
+        this.saldo= 0.0; //Inicializa el saldo en 0
+        this.propietario= propietario;
+        
+    }
+}
