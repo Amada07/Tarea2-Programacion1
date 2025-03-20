9630953d47bcb8de2e64e31c70be30b941363dc4
Amada07 <amadacarcamo938@gmail.com>
2025-03-19 23:03:50 -0600

    Getters and setters de cliente
    se crearon los getters and setters de la clase cliente

--- SistemaBancario/src/sistemabancario/Cliente.java
+++ SistemaBancario/src/sistemabancario/Cliente.java
@@ -20,4 +20,31 @@ public class Cliente {
         this.dpi = dpi;
         this.direccion = direccion; 
  }
+   //Getters and Setters
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDpi() {
+        return dpi;
+    }
+
+    public void setDpi(String dpi) {
+        this.dpi = dpi;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    
+    
 }
