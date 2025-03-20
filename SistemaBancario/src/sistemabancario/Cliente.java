e690333c8d544261af13b981642624926b24d17e
Amada07 <amadacarcamo938@gmail.com>
2025-03-19 21:42:35 -0600

    Se crearon los atributos y el constructor de la clase cliente

---
+++ SistemaBancario/src/sistemabancario/Cliente.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistemabancario;
+
+/**
+ *
+ * @author Amada
+ */
+public class Cliente {
+    // Atributos 
+    private String nombre;
+    private String dpi;
+    private String direccion;
+    
+    //constructor 
+    public Cliente (String nombre,String dpi, String direccion){
+        this.nombre = nombre; 
+        this.dpi = dpi;
+        this.direccion = direccion; 
+ }
+}
